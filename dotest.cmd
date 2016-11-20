60651aaceb5d1bb74f77f000efdd82f8f561a439
dstaulcu <David_Staulcup@hotmail.com>
2016-11-19 21:45:14 -0500

    added test script, changed name of main project file, added support for crash notification

---
+++ dotest.cmd
@@ -0,0 +1,3 @@
+eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"notepad"," 
+eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"notepad"," 
+eventcreate.exe /T Information /ID 1000 /L Application /D ""ProcessName":"notepad"," 
