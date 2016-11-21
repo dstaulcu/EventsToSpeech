971175738023ab518a66ce7af36f62856a20199b
dstaulcu <David_Staulcup@hotmail.com>
2016-11-20 23:30:30 -0500

    update to 1.3.0

--- CreateTestNTLogEvents.cmd
+++ CreateTestNTLogEvents.cmd
@@ -2,10 +2,7 @@
 REM remapped example
 eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
 eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
-eventcreate.exe /T Information /ID 1000 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
-eventcreate.exe /T Information /ID 1000 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
 
 REM default example
 eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"calc.exe"," 
 eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"calc.exe"," 
-eventcreate.exe /T Information /ID 1000 /L Application /D ""ProcessName":"calc.exe"," 
