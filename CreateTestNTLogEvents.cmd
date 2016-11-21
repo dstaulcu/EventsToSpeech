7a476924cb17b826dee66721f20074804f6fb861
dstaulcu <dstaulcu@users.noreply.github.com>
2016-11-20 23:33:56 -0500

    Delete CreateTestNTLogEvents.cmd

--- CreateTestNTLogEvents.cmd
+++
@@ -1,8 +0,0 @@
-
-REM remapped example
-eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
-eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
-
-REM default example
-eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"calc.exe"," 
-eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"calc.exe"," 
