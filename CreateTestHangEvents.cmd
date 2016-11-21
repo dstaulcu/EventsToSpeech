776e8b578c6ea372b189ec191a23f49debf09caf
dstaulcu <David_Staulcup@hotmail.com>
2016-11-20 23:33:31 -0500

    renamed testing batch file

---
+++ CreateTestHangEvents.cmd
@@ -0,0 +1,8 @@
+
+REM remapped example
+eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
+eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"VirtMemTest64.exe"," 
+
+REM default example
+eventcreate.exe /T Information /ID 50 /L Application /D ""ProcessName":"calc.exe"," 
+eventcreate.exe /T Information /ID 51 /L Application /D ""ProcessName":"calc.exe"," 
